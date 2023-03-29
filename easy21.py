2c2bf28d4c1d78ad15a52fcb7c30058ce1659a4c
Deepak H R <deepakhr@Deepaks-MacBook-Air.local>
2023-03-29 23:47:31 +0530

    added MC Control

--- easy21.py
+++ easy21.py
@@ -1,5 +1,4 @@
 import random
-from dataclasses import dataclass, astuple
 
 
 def sample_card():
@@ -8,66 +7,66 @@ def sample_card():
     return color * number
 
 
-@dataclass
-class State:
-    player_sum: int = abs(sample_card())
-    dealer_showing: int = abs(sample_card())
+def dealer_step(state: tuple):
+    has_terminated, (player_sum, dealer_showing), reward = True, state, -1
 
-
-@dataclass
-class StepReturn:
-    has_terminated: bool = False
-    next_state: State = State()
-    reward: int = 0
-
-
-def dealer_step(state: State):
-    dealer_sum = state.dealer_showing
-    while 1 <= dealer_sum < 17:
-        dealer_sum += sample_card()
+    # dealer hits till 17 and sticks after
+    while 1 <= dealer_showing < 17:
+        dealer_showing += sample_card()
+    next_state = player_sum, dealer_showing
 
     # sum is either negative or more than 17
-    next_state = State(state.player_sum, dealer_sum)
-
     # sum not in valid_range -> player wins
-    if next_state.dealer_showing < 1 or next_state.dealer_showing > 21:
-        return StepReturn(has_terminated=True, next_state=next_state, reward=1)
+    if dealer_showing < 1 or dealer_showing > 21:
+        reward = 1
 
     # check between player sum and dealer sum
-    reward = -1
-    if next_state.player_sum > next_state.dealer_showing:
+    elif player_sum > dealer_showing:
         reward = 1
-    elif next_state.player_sum == next_state.dealer_showing:
+
+    # draw when both sums are equal
+    elif player_sum == dealer_showing:
         reward = 0
 
-    return StepReturn(True, next_state, reward)
+    return has_terminated, next_state, reward
 
 
-def step(state: State, action: str):
-    result = StepReturn()
-    if action == "hit":
-        card = sample_card()
-        result.next_state = State(state.player_sum + card, state.dealer_showing)
+class Easy21:
+    @staticmethod
+    def get_starting_state():
+        return abs(sample_card()), abs(sample_card())
 
-        # valid sum, then return with zero reward
-        if 0 < result.next_state.player_sum < 22:
-            return result
+    @staticmethod
+    def step(state: tuple, action: str):
+        has_terminated = False
+        next_state = state
+        reward = 0
+        # action is hit
+        if action == "hit":
+            next_state = next_state[0] + sample_card(), next_state[1]
 
-        # player goes bust and we return -1 reward
-        result.has_terminated = True
-        result.reward = -1
-        return result
+            # player goes bust and we return -1 reward
+            if next_state[0] < 1 or next_state[0] > 21:
+                has_terminated, reward = True, -1
 
-    # when action is stick, we just play out the dealer
-    return dealer_step(state)
+            # valid sum -> return with zero reward
+            return has_terminated, next_state, reward
+
+        # when action is stick, we just play out the dealer
+        return dealer_step(state)
+
+
+def main():
+    env = Easy21()
+    state = env.get_starting_state()
+    print(state)
+    has_terminated, next_state, reward = env.step(state, "hit")
+    if not has_terminated:
+        has_terminated, next_state, reward = env.step(next_state, "hit")
+    if not has_terminated:
+        has_terminated, next_state, reward = env.step(next_state, "stick")
+    print(has_terminated, next_state, reward)
 
 
 if __name__ == "__main__":
-    state = State()
-    print(state)
-    result = step(state, "hit")
-    if not result.has_terminated:
-        result = step(result.next_state, "hit")
-    if not result.has_terminated:
-        result = step(result.next_state, "stick")
-    print(result)
+    main()
