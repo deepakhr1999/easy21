5b0efe35b161205fa9432a2e60fc8b5839fa99fc
Deepak H R <deepakhr@Deepaks-MacBook-Air.local>
2023-04-03 21:57:23 +0530

    added Linear function approximator

--- agent.py
+++ agent.py
@@ -10,6 +10,7 @@ class Agent:
     def __init__(self, no: int = 100, lam: int = 0.5) -> None:
         self.Q = np.zeros((22, 11, 2))
         self.N = np.zeros((22, 11, 2))
+        self.E = np.zeros((22, 11, 2))
         self.no = no
         self.lam = lam
         self.action_names = ["hit", "stick"]
@@ -38,7 +39,7 @@ class Agent:
         state = env.reset()
         action_idx = self.epsilon_greedy_argmax(*state)
         episode = []
-        self.E = np.zeros((22, 11, 2))
+        self.E *= 0.0
         done = False
         while not done:
             player_sum, dealer_showing = state
@@ -100,3 +101,69 @@ class Sarsa(Agent):
         self.E *= self.lam
         self.E[state[0], state[1], action_idx] += 1
         self.Q = self.Q + alpha * delta * self.E
+
+
+class LinearFunctionApproxator(Agent):
+    def __init__(self, no: int = 100, lam: int = 0.5) -> None:
+        super().__init__(no, lam)
+        self.theta = np.random.random(36)
+        self.player_buckets = [[1, 6], [4, 9], [7, 12], [10, 15], [13, 18], [16, 21]]
+        self.dealer_buckets = [[1, 4], [4, 7], [7, 10]]
+        self.epsilon = 5e-2
+        self.alpha = 1e-2
+        self.E = np.zeros_like(self.theta)
+
+    def phi(self, state, action_idx):
+        try:
+            result = np.zeros((6, 3, 2), dtype=float)
+            if state[0] <= 0 or state[1] <= 0:
+                return result.flatten()
+            player_idx = [a <= state[0] <= b for a, b in self.player_buckets]
+            dealer_idx = [a <= state[1] <= b for a, b in self.dealer_buckets]
+            for idx in np.where(player_idx):
+                for jdx in np.where(dealer_idx):
+                    result[idx, jdx, action_idx] = 1
+        except:
+            print(state, action_idx)
+            raise
+        return result.flatten()
+
+    def q_function(self, state, action):
+        return np.dot(self.phi(state, action), self.theta)
+
+    def online_update(
+        self, state, action_idx, reward, next_state, next_action_idx, done
+    ):
+        next_q = 0 if done else self.q_function(next_state, next_action_idx)
+        curr_q = self.q_function(state, action_idx)
+
+        delta = reward + (next_q - curr_q)
+
+        self.E *= self.lam
+        self.E += self.phi(state, action_idx)
+        self.theta += self.alpha * delta * self.E
+
+    def epsilon_greedy_argmax(self, player_sum, dealer_showing):
+        if player_sum < 0 or player_sum >= 22:
+            return 0
+
+        q_values = np.array(
+            [
+                self.q_function((player_sum, dealer_showing), 0),
+                self.q_function((player_sum, dealer_showing), 1),
+            ]
+        )
+
+        # choose random with prob epsilon
+        if random.random() < self.epsilon:
+            return random.choice([0, 1])
+
+        return q_values.argmax()
+
+    def set_q_function(self):
+        for player_sum in range(22):
+            for dealer_showing in range(11):
+                for action_idx in range(2):
+                    self.Q[player_sum, dealer_showing, action_idx] = self.q_function(
+                        (player_sum, dealer_showing), action_idx
+                    )
